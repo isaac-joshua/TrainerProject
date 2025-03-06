871176898a6c6f4af1cb312f8d21b85ea3d2f6ea
Isaac <2393158@cognizant.com>
2025-03-06 18:23:40 +0530

    added (add remove function)

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,15 @@
-$("form").submit(function(event){
+$(document).ready(function() {
+  $("form").submit(function(event){
     event.preventDefault();
-    alert("clicked");
+    const n=$("#name").val();
+    const num=$("#number").val();
+    alert("Name : "+n +"\nNumber : "+num);
+        });  
 
+  $("#add").click(function(){
+    $("#addingtasks").append("<div class='row'> <div class=\"bg-success box p-4 mb-5 col-2 \"> <p>This is a new paragraph.</p> </div></div>");
+  });
+  $("#remove").click(function(){
+    $("#addingtasks div:last").remove();
+  });
 });
