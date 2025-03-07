564500b531e18d4cb39d73dcbdd04dc3f0a0b6a3
Isaac <2393158@cognizant.com>
2025-03-07 12:45:53 +0530

    dynamic addition and removal of tasks

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
-  $("form").submit(function(event){
-    event.preventDefault();
+  $("#add").click(function(event){
     const n=$("#name").val();
-    const num=$("#number").val();
-    alert("Name : "+n +"\nNumber : "+num);
+    $("#addingtasks").append("<div class='row'> <div class=\"bg-success box p-4 mb-5 col-2 \"> <p>"+n+"</p> </div></div>");
+    event.preventDefault();
+    alert("Task : "+n);
         });  
 
-  $("#add").click(function(){
-    $("#addingtasks").append("<div class='row'> <div class=\"bg-success box p-4 mb-5 col-2 \"> <p>This is a new paragraph.</p> </div></div>");
-  });
   $("#remove").click(function(){
-    $("#addingtasks div:last").remove();
+    const num=$("#number").val();
+    $("#addingtasks div").eq(num).remove();
   });
 });
+
